34554d23a629c12721c4e8142fb52e49c3ea1dcf
matheus <matheusaltrao2@gmail.com>
2022-09-29 18:13:49 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+const menu = document.getElementById('menu')
+const list = document.getElementById('list')
+const body = document.querySelector('body')
+menu.onclick = function () {
+  menu.classList.toggle('openmenu')
+  list.classList.toggle('active')
+  body.classList.toggle('hidden')
+}
+
+window.addEventListener('scroll', () => {
+  addBoxShadowHeader()
+  viewButtonToTop()
+})
+
+function addBoxShadowHeader() {
+  const header = document.getElementById('header')
+
+  if (scrollY > 20) {
+    header.classList.add('active')
+  } else {
+    header.classList.remove('active')
+  }
+
+}
+
+$(document).ready(function () {
+  var typed = new Typed('.typing', {
+    strings: ['Artista Visual', 'Desenhista', 'Apaixonado', 'A Criatividade'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  })
+})
+
+
+function scrollToTop() {
+  window.scroll({
+    top: 0,
+  })
+}
+
+function viewButtonToTop() {
+  const buttonTop = document.getElementById('buttonTop')
+
+  if (window.scrollY >= 75) {
+    buttonTop.classList.add('show')
+  } else {
+    buttonTop.classList.remove('show')
+  }
+}
+
+
+
+
+
+
